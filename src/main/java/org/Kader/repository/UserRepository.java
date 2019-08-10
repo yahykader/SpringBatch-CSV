b8b8358758d93dd116db95a2f4c0591a221ddbe9
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-08-10 17:42:16 +0200

    add Batchcongig

--- src/main/java/org/Kader/repository/UserRepository.java
+++ src/main/java/org/Kader/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package org.Kader.repository;
 
-public interface UserRepository {
+import org.Kader.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
